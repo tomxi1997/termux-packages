e7898f68a5f2669d097afe84ce55c6f0613c28e5
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2025-03-16 12:19:15 +0800

    fix githubaction build error

--- root-packages/lxc/build.sh
+++ root-packages/lxc/build.sh
@@ -32,6 +32,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_post_make_install() {
 	# Simple helper script for mounting cgroups.
+        touch /data/data/com.termux/files/usr/lib/liblxc.so
 	install -Dm755 "$TERMUX_PKG_BUILDER_DIR"/lxc-setup-cgroups.sh \
 		"$TERMUX_PREFIX"/bin/lxc-setup-cgroups
 	sed -i "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|" "$TERMUX_PREFIX"/bin/lxc-setup-cgroups
