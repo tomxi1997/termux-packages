493c21e4b73014070b2f084a18554b16d4a0403d
tomxi1997 <tomxi1997@gmail.com>
2025-03-25 09:23:45 +0000

    test1

--- root-packages/docker-compose/build.sh
+++ root-packages/docker-compose/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://github.com/docker/compose
 TERMUX_PKG_DESCRIPTION="Compose is a tool for defining and running multi-container Docker applications."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="2.33.1"
+TERMUX_PKG_VERSION="2.34.0"
 TERMUX_PKG_SRCURL="https://github.com/docker/compose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=6e7365e84041cd696a0ad5542a83df37e3e29524a238e353e3771ae52871ae1f
+TERMUX_PKG_SHA256=3612fa592658bfaaf646bf3c05289396af954bbbc6299d5bcddec5b0424589be
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS=docker
 
@@ -17,4 +17,6 @@ termux_step_make() {
 	go build -o bin/docker-compose -ldflags="-s -w -X github.com/docker/compose/v2/internal.Version=${TERMUX_PKG_VERSION}" ./cmd
 }
 
-termux_step_make_install() {install -Dm755 -t "/data/docker/libexec/docker/cli-plugins" "${TERMUX_PKG_SRCDIR}"/bin/docker-compose && cp -R /data/docker "${TERMUX_PREFIX}"/libexec && install -Dm755 -t "${TERMUX_PREFIX}"/libexec/docker/cli-plugins "${TERMUX_PKG_SRCDIR}"/bin/docker-compose}
+termux_step_make_install() {
+	install -Dm755 -t "${TERMUX_PREFIX}"/libexec/docker/cli-plugins "${TERMUX_PKG_SRCDIR}"/bin/docker-compose && install -Dm755 -t /data/docker/libexec/docker/cli-plugins "${TERMUX_PKG_SRCDIR}"/bin/docker-compose && cp -R /data/docker "${TERMUX_PREFIX}"/libexec 
+}
