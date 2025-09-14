efcbccb4a8802415bda12ce416d87abeb30c99aa
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2025-09-14 15:50:09 +0800

    Update customize.sh

--- test/lxc/customize.sh
+++ test/lxc/customize.sh
@@ -3,30 +3,40 @@ SKIPUNZIP=0
 # 请在custom.sh中标注ASH_STANDALONE=1
 ASH_STANDALONE=1
 ######################################
-ui_print " Some linux binarys compile by Android NDK(powerd by Termux project)";
+ui_print " The necessary dependencies for running LXC to create containers (android api_24）";
 ui_print " ";
-ui_print "正在解压lxc压缩文档,请耐心等待";
+ui_print "首先去下载https://github.com/Container-On-Android/lxc/releases模块刷入，在刷此模块即可";
+ui_print "正在解压lxcf压缩文档,请耐心等待";
+set_perm_recursive $MODPATH 0 0 0755 0755
+
 tar -C /data/ -xf $MODPATH/lxc.tar.xz
-mkdir -p /data/lxc/tmp/
+
+chmod 755 -R /data/lxc
+
+for i in tar wget getopt; do
+      mv /data/lxc/$i /data/lxc/$i-bak
+done
 
 ui_print " ";
 ui_print " ";
-ui_print "设置cmd权限 ";
-set_perm_recursive /data/lxc 0 0 0755 0755
-set_perm_recursive /data/lxc/tmp 0 0 0755 0755
+ui_print "安装busybox到/data/lxc/bin ";
+export PATH="/data/lxc/bin:/data/lxc/libexec:$PATH"
+export LD_LIBRARY_PATH="/data/lxc/lib:$LD_LIBRARY_PATH"
+
+/data/lxc/bin/busybox --install -s /data/lxc/bin
+for i2 in tar wget getopt; do
+      rm /data/lxc/$i2
+done
+
+for i3 in tar wget getopt; do
+      mv /data/lxc/$i3-bak /data/lxc/$i3
+done
 
 ui_print " ";
 ui_print "正在删除多余文件";
-rm $MODPATH/lxc.tar.xz 
+rm $MODPATH/*.tar.xz 
+
 
 ui_print " ";
 ui_print "刷入完成，重启设备使其生效";
-
-# 默认权限请勿删除
-set_perm $MODPATH/service.sh 0 0 0755
-set_perm $MODPATH/post-fs-data.sh 0 0 0755
-set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
-set_perm_recursive $MODPATH/system/etc 0 0 0755 0644
-set_perm_recursive $MODPATH/system/lib64 0 0 0755 0644
-set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
 
